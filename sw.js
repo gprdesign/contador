0fcec0d98bde23e75afb7e0d7cbcc51a59ac98b3
GIO | AURA - ESTUDIO CRIATIVO <giopilrim@gmail.com>
2026-07-17 18:12:38 -0300

    feat: PWA support (sw.js)

--- sw.js
+++ sw.js
@@ -1,13 +1,18 @@
-const CACHE_NAME = 'pwa-v1';
-self.addEventListener('install', evt => {
-  evt.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(['/'])));
-  self.skipWaiting();
+const CACHE_NAME = 'contador-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
 });
-self.addEventListener('activate', evt => {
-  evt.waitUntil(self.clients.claim());
-});
-self.addEventListener('fetch', evt => {
-  evt.respondWith(
-    caches.match(evt.request).then(r => r || fetch(evt.request))
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
   );
 });
